3417cf8bb37edc7ef72f354f86195a3ba7f90add
Babatunde <d2ndeayoade@gmail.com>
2024-07-16 16:43:21 +0100

    i learnt a brief introduction to C language. i aslo wrote my first c program even though i am still trying to understand the syntax, i was able to write and run my first c program succesfuly. lets go........

---
+++ Learning_process.c
@@ -0,0 +1,75 @@
+// Learn C
+// C is a general-purpose programming language that has been widely used for over 50 years.
+
+// C is very powerful; it has been used to develop operating systems, databases, applications, etc.
+
+
+// C Introduction >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// What is C?
+// C is a general-purpose programming language created by Dennis Ritchie at the Bell Laboratories in 1972.
+
+// It is a very popular language, despite being old. The main reason for its popularity is because it is a fundamental language in the field of computer science.
+
+// C is strongly associated with UNIX, as it was developed to write the UNIX operating system.
+
+
+
+// Why Learn C?
+// It is one of the most popular programming languages in the world
+// If you know C, you will have no problem learning other popular programming languages such as Java, Python, C++, C#, etc, as the syntax is similar
+// C is very fast, compared to other programming languages, like Java and Python
+// C is very versatile; it can be used in both applications and technologies
+
+
+// Difference between C and C++
+// C++ was developed as an extension of C, and both languages have almost the same syntax
+// The main difference between C and C++ is that C++ support classes and objects, while C does not
+
+
+
+// C Get Started >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Get Started With C
+// To start using C, you need two things:
+
+// A text editor, like Notepad, to write C code
+// A compiler, like GCC, to translate the C code into a language that the computer will understand
+// There are many text editors and compilers to choose from. In this tutorial, i will use VScode as the text editor and GCC as the compiler.
+
+
+
+// C Install IDE
+// An IDE (Integrated Development Environment) is used to edit AND compile the code.
+
+// Popular IDE's include Code::Blocks, Eclipse, and Visual Studio. These are all free, and they can be used to both edit and debug C code.
+
+// Note: Web-based IDE's can work as well, but functionality is limited.
+
+// I will use visual studio in this tutorial.
+
+// you can also use Code::Blocks, which we believe is a good place to start.
+
+// You can find the latest version of Codeblocks at http://www.codeblocks.org/. Download the mingw-setup.exe file, which will install the text editor with a compiler.
+
+
+// C Quickstart
+// Let's create our first C file.
+
+// Open Codeblocks and go to File > New > Empty File.
+
+// Write the following C code and save the file as myfirstprogram.c 
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
+
+// Don't worry if you don't understand the code above - we will discuss it in detail in later chapters. For now, focus on how to run the code.
+// now run the code and you should get this output.
+
+// Hello World!
+
+// Congratulations! You have written and executed your first C program.
